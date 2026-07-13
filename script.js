b86fec60888bcb7baa9be93463fc86846f901fa0
Moussaoui Sarah <moussaouisarah78@gmail.com>
2026-07-13 19:18:32 +0100

    Implement drag-and-drop functionality for Kanban cards

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
-ini
+const cards = document.querySelectorAll('.card') ;
+const lists = document.querySelectorAll('.list') ;
+
+for (const card of cards ) {
+    card.addEventListener('dragstart' , dragStart) ;
+    card.addEventListener('dragend' , dragEnd) ;
+}
+
+
+for (const list of lists ) {
+    list.addEventListener('dragover', dragOver) ;
+    list.addEventListener('dragenter', dragEnter) ;
+    list.addEventListener('dragleave', dragLeave) ;
+    list.addEventListener('drop', dragDrop) ;
+}
+
+
+function dragStart(e){
+    e.dataTransfer.setData("text/plain" , this.id) ;
+     console.log("the drag started") ;
+
+}
+
+function dragEnd() {
+    console.log("the drag ended") ;
+}
+
+function dragOver(e) {
+    e.preventDefault() ;
+    /* the event works once the card is iver a draged area */
+    console.log('drag over') ;
+}
+
+function dragEnter(e) {
+    e.preventDefault() ;
+    this.classList.add("over") ;
+}
+
+
+function dragLeave(e) {
+    this.classList.remove("over") ;
+}
+
+function dragDrop(e) {
+    const id = e.dataTransfer.getData('text/plain') ;
+    const card = document.getElementById(id) ;
+
+    this.appendChild(card) ;
+    this.classList.remove('over') ;
+}
+
